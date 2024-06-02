be5ef7b7ebf4cd3c284f4b899bfa1118b025cde6
uefikk <uefimova81@gmail.com>
2024-06-02 13:26:34 +0700

    Аналоговые часы

--- lab8/time.js
+++ lab8/time.js
@@ -2,7 +2,13 @@ function showTime() {
     let today = new Date();
     let currentTime = today.toLocaleTimeString('ru-RU');
     document.getElementById('time').innerHTML = currentTime;
-    let seconds = today.getSeconds();
-    document.getElementById('seconds').innerHTML = seconds;
+    const seconds = today.getSeconds();
+    const secondsDisplay = document.getElementById('seconds');
+    const secondHand = document.getElementById('second-hand');
+
+    secondsDisplay.innerHTML = seconds.toString().padStart(2, '0');
+
+    const secondsDegree = ((seconds / 60) * 360) + 90; // Смещение на 90 градусов для начала с 12:00
+    secondHand.style.transform = `rotate(${secondsDegree}deg)`;
 }
 setInterval(showTime, 1000);
