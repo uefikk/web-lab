2a20b04ea39a03a9f66af8294d89da5950336990
uefikk <uefimova81@gmail.com>
2024-06-02 12:28:00 +0700

    Вывод текущей даты

--- lab8/lab8.js
+++ lab8/lab8.js
@@ -1,6 +1,7 @@
 function showDate() {
     let out = document.getElementById('current-date');
     let today = new Date();
+    const weekDays = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
     out.innerHTML = `
         <div class="frame">
             <div>Дата и время для локали России: ${today.toLocaleString('ru-RU')}</div>
@@ -9,6 +10,11 @@ function showDate() {
             <div>Дата и время для локали Албании: ${today.toLocaleString('sq-AL')}</div>
             <div>Дата и время для локали Уэльс: ${today.toLocaleString('cy-GB')}</div>
             <div>Дата и время для локали Греции: ${today.toLocaleString('el-GR')}</div>
+            <div>Текущий год: ${today.getFullYear()}</div>
+            <div>Текущий месяц: ${today.getMonth()}</div>
+            <div>Текущая дата: ${today.getDate()}</div>
+            <div>День недели: ${weekDays[today.getDay()]}</div>
         </div>
+
     `;
 }
